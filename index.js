d49270226e9558a408407901c12ddb2a29b9dff8
Wade Johnson <wadej93@gmail.com>
2019-08-25 16:55:42 -0700

    Add secrets file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mysql = require('mysql2');
+const secrets = require('./secrets.json');
 
 // create the connection to database
-//Password is left empty
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'mylomylo',
+  user: secrets.MYSQL_USER,
+  password: secrets.MYSQL_PASS,
   database: 'dam'
 });
 
