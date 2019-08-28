c10778744ca9b95a54782d6f5ad2c8dd37bd66c3
weejamminpdx <weejamminpdx@gmail.com>
2019-08-27 20:52:23 -0700

    Commit form folder

---
+++ Form/index.js
@@ -0,0 +1,13 @@
+var password = document.getElementById("password")
+ , confirmPassword = document.getElementById("confirmPassword");
+
+function validatePassword() {
+    if(password.value != confirmPassword.value) {
+        confirmPassword.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm.password.setCustomValidity('');
+    }
+}
+
+password.onchange = validatePassword;
+confirmPassword.onkeyup = validatePassword;
