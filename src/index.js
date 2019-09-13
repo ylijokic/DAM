29da9eb2c0e7a574f7e9281eb4d84f65a450da4b
Charlie Ylijoki <ylijokic@oregonstate.edu>
2019-09-13 09:47:35 -0700

    Revert "Added Express-Handlebars/Created Views Directory"
    This reverts commit e9fcafff5d3edc2488565b90b8f14eca87a59770.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const mysql = require('./dbconn')
 
 const app = express()
-const hbs = expressHandlebars.create({ defaultLayout: 'main' })
 
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
+const mysql = require('./dbconn')
 
 // Router Modules
 const user = require('./routes/user')
 const asset = require('./routes/asset')
 const folder = require('./routes/folder')
-const assetFolder = require('./routes/asset_folder_mapping')
 
 // Load Router Modules in App
 app.use('/user', user)
 app.use('/asset', asset)
 app.use('/folder', folder)
-app.use('/asset_folder', assetFolder)
 
 // Load Database into App
 app.set('mysql', mysql)
 
-app.use((req, res) => {
+app.use(function(req, res) {
   res.status(404)
-  res.render('404')
 })
 
-app.use((err, req, res) => {
+app.use(function(err, req, res, next) {
+  console.error(err.stack)
   res.status(500)
-  res.render('500')
 })
 
 app.listen('3000', () => {
